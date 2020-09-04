05626bd5f4fb3981e2f97d37fe4181856d1124fa
Marcelo Seiji <ikuta.marcelo@gmail.com>
2020-09-03 22:15:02 -0300

    add scss color variables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './views/home/home.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 import { EstablishmentListComponent } from './views/establishment-list/establishment-list.component';
 import { NavComponent } from './views/nav/nav.component';
 
@@ -15,16 +16,17 @@ import { NavComponent } from './views/nav/nav.component';
     AppComponent,
     HomeComponent,
     EstablishmentListComponent,
-    NavComponent
+    NavComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatListModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
