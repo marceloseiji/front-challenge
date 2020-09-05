93f7a785b2e7f977774e64cb13c1974d09f0526f
Marcelo Seiji <ikuta.marcelo@gmail.com>
2020-09-05 17:48:22 -0300

    added a model to establushment

---
+++ src/app/shared/model/responseEstablishment.model.ts
@@ -0,0 +1,13 @@
+export class ResponseEstablishment {
+  address: string;
+  email: string;
+  guid: string;
+  id: string;
+  index: 0
+  latitude: string;
+  longitude: string;
+  name: string;
+  phone: string;
+  picture: string;
+  registered: string;
+}
