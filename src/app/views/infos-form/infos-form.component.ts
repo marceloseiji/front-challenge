c9e94c8e0ab425242ae4773159597d53189c2dc7
Marcelo Seiji <ikuta.marcelo@gmail.com>
2020-09-06 19:04:18 -0300

    form data bind to update item complete

--- src/app/views/infos-form/infos-form.component.ts
+++ src/app/views/infos-form/infos-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { EstablishmentService } from '../../shared/service/establishment.service';
 import { ResponseEstablishment } from '../../shared/model/responseEstablishment.model';
+import { NgForm } from '@angular/forms';
+import { LocalStorageService, SessionStorageService } from 'ngx-webstorage';
 
 @Component({
   selector: 'app-infos-form',
@@ -9,6 +11,7 @@ import { ResponseEstablishment } from '../../shared/model/responseEstablishment.
 })
 export class InfosFormComponent implements OnInit {
 
+  //Item original recebido
   itemReceived: ResponseEstablishment = {
     address: "",
     email: "",
@@ -23,33 +26,79 @@ export class InfosFormComponent implements OnInit {
     registered: ""
   };
 
-  $street: any;
-  $city: any;
-  $state: any;
-  $zipCode: any;
+  //Informações adicionais do item recebido
+  otherInfos = {
+    $street: "",
+    $city: "",
+    $state: "",
+    $zipCode: "",
+    $banco: "",
+    $tipoConta: "",
+    $cpfCnpj: "",
+    $agencia: "",
+    $agenciaDigito: "",
+    $contaNumero: "",
+    $contaDigito: "",
+    $saqueAutomatico: "",
+  }
+
+  $bancos = [
+    {nome: 'Banco do Brasil'},
+    {nome: 'Bradesco'},
+    {nome: 'Itaú'}
+  ];
+
+  $contas = [
+    {tipo: 'Conta corrente'},
+    {tipo: 'Poupança'}
+  ];
+
+  $saqueOpcoes = [
+    {tipo: 'Sim'},
+    {tipo: 'Não'}
+  ];
 
   constructor(
     private establishmentService: EstablishmentService,
+    private storage:LocalStorageService
   ) { }
+
+  public changeEstablishment() {
+    const newAddress = `${this.otherInfos.$street},${this.otherInfos.$city},${this.otherInfos.$state},${this.otherInfos.$zipCode},
+    `;
+
+    let updatedItem = {
+      address: newAddress,
+      id: this.itemReceived.id,
+      name: this.itemReceived.name,
+      banco: this.otherInfos.$banco,
+      tipoConta: this.otherInfos.$tipoConta,
+      cpfCnpj: this.otherInfos.$cpfCnpj,
+      agencia: this.otherInfos.$agencia,
+      agenciaDigito: this.otherInfos.$agenciaDigito,
+      contaNumero: this.otherInfos.$contaNumero,
+      contaDigito: this.otherInfos.$contaDigito,
+      saqueAutomatico: this.otherInfos.$saqueAutomatico
+    }
+    this.storage.store('Establishment Item' + updatedItem.id, updatedItem);
+    console.log("Novo item: ", updatedItem);
+  }
 
   ngOnInit(): void {
     //item recebido do establishment.service itemSend que é um Subject
     this.establishmentService.itemSend.subscribe(item => {
       this.itemReceived = item
-      
+
       const cut = this.itemReceived.address.split(",");
 
-      this.$street = cut[0];
-      this.$city = cut[1];
-      this.$state = cut[2];
-      this.$zipCode = cut[3];
-
-      // console.log("street: ", this.$street)
-      // console.log("city: ", this.$city)
-      // console.log("state: ", this.$state)
-      // console.log("zipCode: ", this.$zipCode)
+      this.otherInfos.$street = cut[0];
+      this.otherInfos.$city = cut[1];
+      this.otherInfos.$state = cut[2];
+      this.otherInfos.$zipCode = cut[3];
       
+      document.getElementById("sendSubmit").classList.remove('disabled');
+      document.getElementById("sendSubmit").removeAttribute("disabled"); 
+
     });
   }
-
 }
